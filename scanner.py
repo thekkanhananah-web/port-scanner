b797bd2f37f668eb21d5d2824f152f1dcf8c81f1
Hanana <thekkanhananah@gmail.com>
2026-06-08 21:04:38 +0530

    Add files via upload
    Network security is an important aspect of modern computing systems. Identifying open ports and active
    services helps administrators monitor and secure their networks. This project implements a simple Port
    Scanner using Python and the Nmap library. The application provides a user-friendly command-line
    interface that allows users to perform SYN ACK scans, UDP scans, and Comprehensive scans on a specified
    IP address. The scanner displays information such as host status, available protocols, and open ports. This
    project demonstrates the integration of Python programming with network security tools and provides a
    practical introduction to cybersecurity concepts.

---
+++ scanner.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+import nmap
+
+scanner = nmap.PortScanner()
+
+print("Welcome, this is a simple nmap automation tool")
+print("<--------------------------------------------------->")
+
+ip_addr = input("Please enter the IP address you want to scan: ")
+print("The IP you entered is: ", ip_addr)
+
+resp = input("""\nPlease enter the type of scan you want to run
+1) SYN ACK Scan
+2) UDP Scan
+3) Comprehensive Scan\n""")
+print("You have selected option: ", resp)
+
+if resp == '1':
+    print("Nmap Version: ", scanner.nmap_version())
+    scanner.scan(ip_addr, '1-1024', '-v -sS')
+    print(scanner.scaninfo())
+    print("IP Status: ", scanner[ip_addr].state())
+
+    protocols = scanner[ip_addr].all_protocols()
+    print("Protocols:", protocols)
+
+    if 'tcp' in protocols:
+        print("Open Ports:", list(scanner[ip_addr]['tcp'].keys()))
+    else:
+        print("No TCP ports found.")
+elif resp == '2':
+    print("Nmap Version: ", scanner.nmap_version())
+    scanner.scan(ip_addr, '1-1024', '-v -sU')
+    print(scanner.scaninfo())
+    print("IP Status: ", scanner[ip_addr].state())
+
+    protocols = scanner[ip_addr].all_protocols()
+    print("Protocols:", protocols)
+
+    if 'udp' in protocols:
+        print("Open Ports:", list(scanner[ip_addr]['udp'].keys()))
+    else:
+        print("No UDP ports found.")
+elif resp == '3':
+    print("Nmap Version: ", scanner.nmap_version())
+    scanner.scan(ip_addr, '1-1024', '-v -sS -sV -sC -A -O')
+    print(scanner.scaninfo())
+    print("IP Status: ", scanner[ip_addr].state())
+
+    protocols = scanner[ip_addr].all_protocols()
+    print("Protocols:", protocols)
+
+    if 'tcp' in protocols:
+        print("Open Ports:", list(scanner[ip_addr]['tcp'].keys()))
+    else:
+        print("No TCP ports found.")
+else:
+    print("Please enter a valid option")
